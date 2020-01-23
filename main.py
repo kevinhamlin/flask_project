c1fa6e6b35c8873a67bfaebc2b1a6f61e95afdfd
Amplexis <kevin.hamlin@gmail.com>
2020-01-23 16:01:24 -0500

    updated home.html to include a code block which loops through blog posts

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
