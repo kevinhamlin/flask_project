2cdd8d98d4cfc83e0c989b934cf4eef8016eb709
Amplexis <kevin.hamlin@gmail.com>
2020-01-23 15:40:25 -0500

    Added about route

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello World!</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
