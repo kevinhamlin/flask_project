3d155cd8410eed17d928c2ab4171d904a07c133a
Amplexis <kevin.hamlin@gmail.com>
2020-01-23 17:51:32 -0500

    Added registration page

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for
 from imports.posts import posts
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '68cf826fbd9c2a5dbdd5dc2bafc7134e'
 
 @app.route("/")
 @app.route("/home")
@@ -12,6 +13,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return  render_template('login.html', title="Login", form=form)
+
 
 
 if __name__ == '__main__':
