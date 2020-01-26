1f9d41da123df9bc7f2d105643863b6d445fab63
Amplexis <kevin.hamlin@gmail.com>
2020-01-26 12:30:51 -0500

    added user.password encryption and writing of new users to db

--- main/__init__.py
+++ main/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '68cf826fbd9c2a5dbdd5dc2bafc7134e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
 
 from main import routes
--- main/routes.py
+++ main/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect
 from main.forms import RegistrationForm, LoginForm
 from main.imports.posts import posts
-from main import app
+from main import app, db, bcrypt
+from main.models.models import User
 
 
 @app.route("/")
@@ -17,8 +18,14 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        #hash the password they gave us
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        #create a new instance of a user
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created. You are now able to login.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
