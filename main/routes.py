9eb8e71d100d40b8b65e336f63b91b45c8c54656
Amplexis <kevin.hamlin@gmail.com>
2020-01-26 14:23:35 -0500

    Added user image functionality along with a default user pic.

--- main/routes.py
+++ main/routes.py
@@ -57,4 +57,5 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    image_file = url_for('static', filename='profile_pictures/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file)
