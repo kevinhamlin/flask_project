b6e1d506db0251eafa11a261a41e4ee846a45e54
Amplexis <kevin.hamlin@gmail.com>
2020-01-26 13:09:48 -0500

    Added user_login functionality

--- main/routes.py
+++ main/routes.py
@@ -3,6 +3,7 @@ from main.forms import RegistrationForm, LoginForm
 from main.imports.posts import posts
 from main import app, db, bcrypt
 from main.models.models import User
+from flask_login import login_user, current_user
 
 
 @app.route("/")
@@ -16,6 +17,9 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         #hash the password they gave us
@@ -30,10 +34,14 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'hhhhhhhhhh':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
             flash('Login failed. Please check email and password.', 'danger')
