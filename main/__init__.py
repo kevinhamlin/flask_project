437ee2ad3aee000a5b1f029b40c9978e9709bcac
Amplexis <kevin.hamlin@gmail.com>
2020-01-27 18:16:03 -0500

    added Mail imports and mail credential environment variables

--- main/__init__.py
+++ main/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '68cf826fbd9c2a5dbdd5dc2bafc7134e'
@@ -15,5 +17,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
 
 from main import routes
