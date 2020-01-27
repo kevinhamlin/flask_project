7c1c2ed69e5452969184fac58d576465dd1efd45
Amplexis <kevin.hamlin@gmail.com>
2020-01-27 17:59:25 -0500

    added reset_token route

--- main/routes.py
+++ main/routes.py
@@ -156,3 +156,16 @@ def reset_request():
         return redirect(url_for('home'))
     form = RequestResetForm()
     return render_template('reset_request.html', title="Reset Password", form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('Your token is invalid or expired.', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form=form)
+
