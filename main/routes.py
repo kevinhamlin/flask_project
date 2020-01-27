10112586a22ecf2872fac3e850623b174d7972a7
Amplexis <kevin.hamlin@gmail.com>
2020-01-27 18:19:10 -0500

    added functionality to resetpassword route.

--- main/routes.py
+++ main/routes.py
@@ -155,6 +155,11 @@ def reset_request():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password', 'info')
+        return redirect(url_for('login'))
     return render_template('reset_request.html', title="Reset Password", form=form)
 
 
@@ -169,3 +174,6 @@ def reset_token(token):
     form = ResetPasswordForm()
     return render_template('reset_token.html', title='Reset Password', form=form)
 
+
+def send_reset_email(user):
+    pass
