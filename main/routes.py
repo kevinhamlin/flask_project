c6458fd7800f5b7224e412df56fb5906f1c600e4
Amplexis <kevin.hamlin@gmail.com>
2020-01-27 14:54:59 -0500

    update_post method now fills forms with post title and content, if form is valid on submit, commit post data and redirect to post page.

--- main/routes.py
+++ main/routes.py
@@ -110,14 +110,21 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
-    form.title.data = post.title
-    form.content.data = post.content
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has successfully been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
 
