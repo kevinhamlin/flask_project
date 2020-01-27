89e8774bca56a2b4a876d6dbd212061da752a39a
Amplexis <kevin.hamlin@gmail.com>
2020-01-27 17:29:46 -0500

    added get_reset_token and verify_reset_token methods to User class

--- main/models/models.py
+++ main/models/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from main import db, login_manager
+from main import db, login_manager, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,9 +15,22 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+
+    def get_reset_token(self, expires_seconds=1800):
+        serial = Serializer(app.config['SECRET_KEY'], expires_seconds)
+        return serial.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        serial = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = serial.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
 
 
 
