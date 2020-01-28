e94f3753d7d11e5b4777c369753185926ca5f3ba
Amplexis <kevin.hamlin@gmail.com>
2020-01-28 13:20:17 -0500

    imported Config class

--- main/__init__.py
+++ main/__init__.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from main.config import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
@@ -15,8 +15,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'danger'
-
-
 
 mail = Mail(app)
 
