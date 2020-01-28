23f70ea34e2313ad6f98c81a745160d5b71ad82a
Amplexis <kevin.hamlin@gmail.com>
2020-01-28 12:26:39 -0500

    moved all 'routes' and utility functions into new corrosponding routes file in correct directory (users, posts, primary)

---
+++ main/primary/routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+primary = Blueprint('primary', __name__)
+
+
+@primary.route("/")
+@primary.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
+
+
+@primary.route("/about")
+def about():
+    return render_template('about.html', title='About')
---
+++ main/users/utilities.py
@@ -0,0 +1,23 @@
+
+def save_picture(form_picture):
+    random_hex_value = secrets.token_hex(8)
+    _, file_ext = os.path.splitext(form_picture.filename)
+    profile_picture_filename = random_hex_value + file_ext
+    profile_pic_path = os.path.join(app.root_path, 'static/profile_pictures', profile_picture_filename)
+
+    picture_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(picture_size)
+    i.save(profile_pic_path)
+    return profile_picture_filename
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    message = Message('Password Reset Request', sender='kevin.hamlin@gmail.com', recipients=[user.email])
+    message.body = f'''To reset your password, please click the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not request this password reset, please ignore this email.
+'''
+    mail.send(message)
